1e66a543ed3020c47ace912d9e7541093d00661d
Tiago Matias <tiagomatias072@gmail.com>
2025-09-16 09:19:00 +0000

    background da page login-v3

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -61,7 +61,7 @@ export default function LoginPage() {
         <div className="text-center mb-8">
           <Link href="/" className="inline-flex items-center gap-2 text-2xl font-bold text-gray-900">
             <img src="/42skillar.png" alt="42Skillar Logo" className="h-10 w-10 object-contain" />
-            Skillar
+            <p>Skillar</p>
             <p className="text-center text-sm text-italic text-gray-300">Arena dos Campeões</p>
           </Link>
         </div>
